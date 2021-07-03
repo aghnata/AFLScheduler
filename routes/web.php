b092f3a9f8f813bf44c3a504a7aadbddf391e5bd
Aghnat Atqiya <aghnat.atqiya@gmail.com>
2021-07-03 07:23:19 +0700

    maintenance web afl

--- routes/web.php
+++ routes/web.php
@@ -11,31 +11,31 @@
 |
 */
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/admin', function () {
-        return view('dashboard');
-    });
-    Route::resource('/jadwal_AFLers', 'PenjadwalanController');
-    Route::get('/Schedule/All', 'ScheduleController@All');
-    Route::post('/Schedule/SortingSchedule', 'ScheduleController@SortingSchedule');
-    Route::post('/Schedule/ChangePaymentStatus', 'ScheduleController@ChangePaymentStatus');
-    Route::post('/Schedule/Store', 'ScheduleController@Store');
-    Route::post('/Schedule/Delete', 'ScheduleController@Delete');
+// Route::group(['middleware' => 'auth'], function () {
+//     Route::get('/admin', function () {
+//         return view('dashboard');
+//     });
+//     Route::resource('/jadwal_AFLers', 'PenjadwalanController');
+//     Route::get('/Schedule/All', 'ScheduleController@All');
+//     Route::post('/Schedule/SortingSchedule', 'ScheduleController@SortingSchedule');
+//     Route::post('/Schedule/ChangePaymentStatus', 'ScheduleController@ChangePaymentStatus');
+//     Route::post('/Schedule/Store', 'ScheduleController@Store');
+//     Route::post('/Schedule/Delete', 'ScheduleController@Delete');
 
 
-});
+// });
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tampilan', function () {
-    return view('tampilan');
-});
+// Route::get('/tampilan', function () {
+//     return view('tampilan');
+// });
 
-Route::resource('/afl2', 'AFLController');
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/verify/{token}/{id}', 'Auth\RegisterController@verify_register');
-Route::get('/listaflee', 'AfleeController@ShowAflerByAflee');
-Route::get('/listafler', 'AflerController@ShowAfleeByAfler');
+// Route::resource('/afl2', 'AFLController');
+// Auth::routes();
+// Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/verify/{token}/{id}', 'Auth\RegisterController@verify_register');
+// Route::get('/listaflee', 'AfleeController@ShowAflerByAflee');
+// Route::get('/listafler', 'AflerController@ShowAfleeByAfler');
